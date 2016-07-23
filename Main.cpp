152aaff6749fdcf3a904741350dff7eacc9de7b6
Sam Bazley <samb1999@hotmail.co.uk>
2016-07-23 14:11:48 +0100

    Added support for new event system

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,8 @@
 
 #include <SME_window.h>
 #include <SME_core.h>
-#include <SME_events.h>
+#include <SME_event.h>
+#include <SME_ui_events.h>
 #include <SME_render.h>
 #include <SME_pipeline.h>
 #include <SME_config.h>
@@ -19,45 +20,60 @@
 SME::Level::Level level;
 
 void update() {
-    SME::Events::Event event;
-    while (SME::Events::getEvent(event)) {
-        switch (event.type) {
-            case SME::Events::SME_WINDOW:
-                if (event.windowEvent.event == SME::Events::SME_WINDOW_CLOSE) {
-                    std::cout << "exit" << std::endl;
-                    SME::Core::stop();
-                } else if (event.windowEvent.event == SME::Events::SME_WINDOW_RESIZE) {
-                    std::cout << "new dimensions: " << +event.windowEvent.width << ", " << +event.windowEvent.height << std::endl;
-                } else if (event.windowEvent.event == SME::Events::SME_WINDOW_MAXIMISE) {
-                    std::cout << "maximise" << std::endl;
-                } else if (event.windowEvent.event == SME::Events::SME_WINDOW_MINIMISE) {
-                    std::cout << "minimise" << std::endl;
-                }
-                break;
-            case SME::Events::SME_KEYBOARD:
-                if (event.keyboardEvent.event == SME::Events::SME_KEYBOARD_KEYDOWN) {
-                    std::cout << "d repeated: " << event.keyboardEvent.repeated << std::endl;
-                } else if (event.keyboardEvent.event == SME::Events::SME_KEYBOARD_KEYUP) {
-                    std::cout << "u repeated: " << event.keyboardEvent.repeated << std::endl;
-                }
-            case SME::Events::SME_MOUSE:
-                if (event.mouseEvent.event == SME::Events::SME_MOUSE_MOVE) {
-//                    std::cout << "mouse move: " << event.mouseEvent.x << ", " << event.mouseEvent.y << std::endl;
-//                    std::cout << "mouse rel: " << event.mouseEvent.xdelta << ", " << event.mouseEvent.ydelta << std::endl;
-                } else if (event.mouseEvent.event == SME::Events::SME_MOUSE_MOUSEDOWN) {
-                    std::cout << "mouse down: " << event.mouseEvent.button << std::endl;
-                } else if (event.mouseEvent.event == SME::Events::SME_MOUSE_MOUSEUP) {
-                    std::cout << "mouse up: " << event.mouseEvent.button << std::endl;
-                } else if (event.mouseEvent.event == SME::Events::SME_MOUSE_WHEEL) {
-                    std::cout << "scroll: " << +event.mouseEvent.scroll << std::endl;
-                }
-        }
-    }
-    
     level.update();
 }
 
+class WindowListener : public SME::Event::EventListener {
+    void onEvent(SME::Event::Event &event) {
+        if (event.getType() == SME::Event::UI::UIEventKeys::WindowCloseEvent) {
+            std::cout << "exit" << std::endl;
+            SME::Core::stop();
+        }
+    }
+};
+
+class KeyboardListener : public SME::Event::EventListener {
+    void onEvent(SME::Event::Event &event) {
+        if (event.getType() == SME::Event::UI::UIEventKeys::KeyDownEvent) {
+            SME::Event::UI::KeyDownEvent &kde = static_cast<SME::Event::UI::KeyDownEvent&>(event);
+            std::cout << kde.keycode << " d, repeated " << kde.repeated << std::endl;
+        } else if (event.getType() == SME::Event::UI::UIEventKeys::KeyUpEvent) {
+            SME::Event::UI::KeyUpEvent &kue = static_cast<SME::Event::UI::KeyUpEvent&>(event);
+            std::cout << kue.keycode << " u" << std::endl;
+        }
+    }
+};
+
+class MouseListener : public SME::Event::EventListener {
+    void onEvent(SME::Event::Event &event) {
+        SME::Event::UI::MouseEvent &me = static_cast<SME::Event::UI::MouseEvent&>(event);
+        std::cout << me.x << ", " << me.y << std::endl;
+    }
+};
+
 int main(int argc, char** argv) {
+    SME::Core::init();
+
+    WindowListener wl;
+    SME::Event::registerListener(&wl,{
+        SME::Event::UI::UIEventKeys::WindowCloseEvent,
+        SME::Event::UI::UIEventKeys::WindowMaximiseEvent,
+        SME::Event::UI::UIEventKeys::WindowMinimiseEvent,
+        SME::Event::UI::UIEventKeys::WindowResizeEvent
+    });
+    
+    KeyboardListener kl;
+    SME::Event::registerListener(&kl,{
+        SME::Event::UI::UIEventKeys::KeyDownEvent,
+        SME::Event::UI::UIEventKeys::KeyUpEvent
+    });
+    
+    MouseListener ml;
+    SME::Event::registerListener(&ml,{
+        SME::Event::UI::UIEventKeys::MouseDownEvent,
+        SME::Event::UI::UIEventKeys::MouseUpEvent
+    });
+
     SME::Config::loadFile("test.cfg");
     SME::Config::registerValue("a", "test");
     SME::Config::registerValue("b", 12);
@@ -65,29 +81,29 @@ int main(int argc, char** argv) {
     SME::Config::registerValue("d", 9223372036854775807);
     SME::Config::registerValue("e", -123);
     SME::Config::registerValue("f", true);
-    
+
     std::cout << "f: " << SME::Config::getValue<bool>("f") << std::endl;
-    
+
     SME::Config::saveFile("test.cfg");
-    
+
     SME::XML::XMLBase xml = SME::XML::parseXML("model.dae");
     std::cout << "attrib contents = " << SME::XML::getFirstTag(xml, "COLLADA.library_effects.effect.profile_COMMON.technique.phong.emission.color")->contents << std::endl;
-    
+
     level.setParameter<std::string>("test", "test str");
     level.setParameter("test1", 12);
     level.addEntity("TestEntity");
-    
+
     std::cout << "test: " << level.getParameter<std::string>("test") << ", test1: " << level.getParameter<int>("test1") << std::endl;
-    
-    SME::Core::init();
+
     SME::Window::create(1920 * 3 / 4, 1080 * 3 / 4, "Test", SME_WINDOW_RESIZABLE);
     SME::TestPipeline* pipeline = new SME::TestPipeline();
     SME::Render::addPipeline(pipeline);
-    if(!SME::Render::init("Test", VK_MAKE_VERSION(0, 0, 1))){
+    if (!SME::Render::init("Test", VK_MAKE_VERSION(0, 0, 1))) {
         std::cerr << "Couldn't initialise rendering lib!" << std::endl;
         return -1;
     }
     SME::Core::addLoopUpdateHook(update);
+
     SME::Core::start();
     return 0;
 }
